f9200efc7a4feb455b9c3772dfac3de41684e96f
Giuliana <giulyferto@gmail.com>
2021-01-07 08:51:18 -0300

    Se crea la clase UserController y UserView

---
+++ src/main/java/ar/com/ada/second/online/maven/controller/UserController.java
@@ -0,0 +1,18 @@
+package ar.com.ada.second.online.maven.controller;
+
+//import ar.com.ada.second.online.maven.view.MainView;
+//import ar.com.ada.second.online.maven.view.UserView;
+
+public class UserController {
+    private static  UserController userController;
+    private MainView mainView = MainView.getInstance();
+    private UserView userView = UserView.getInstance();
+
+    private UserController() {
+
+    }
+    public static UserController getInstance() {
+        if (userController == null) userController = new UserController();
+        return userController;
+    }
+}
---
+++ src/main/java/ar/com/ada/second/online/maven/view/UserView.java
@@ -0,0 +1,14 @@
+package ar.com.ada.second.online.maven.view;
+
+public class UserView {
+private static UserView userView;
+
+private UserView(){
+
+}
+public static UserView getInstance(){
+    if (userView == null) userView = new UserView();
+    return userView;
+}
+
+}
