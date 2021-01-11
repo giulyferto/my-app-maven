4e96f2b4a974a0a1cd04cf63072d19f00c1c492e
Giuliana <giulyferto@gmail.com>
2021-01-11 07:25:10 -0300

    Se crean paquetes model y config con .gitkeep

--- src/main/java/ar/com/ada/second/online/maven/controller/UserController.java
+++ src/main/java/ar/com/ada/second/online/maven/controller/UserController.java
@@ -1,7 +1,7 @@
 package ar.com.ada.second.online.maven.controller;
 
-//import ar.com.ada.second.online.maven.view.MainView;
-//import ar.com.ada.second.online.maven.view.UserView;
+import ar.com.ada.second.online.maven.view.MainView;
+import ar.com.ada.second.online.maven.view.UserView;
 
 public class UserController {
     private static  UserController userController;
